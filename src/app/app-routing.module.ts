74c87fcadcb2a6aa62303e8d4c7017a08ef851a7
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-10-05 01:13:11 -0300

    Criação nav-bar e not-found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/components/not-found';
 import { CourseListComponent } from './core/course/pages/course-list';
-import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'courses', component: CourseListComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { CourseListComponent } from './core/course/pages/course-list';
 import { StarComponent } from './core/components/star';
 import { FormsModule } from '@angular/forms';
 import { TransformCodePipe } from './core/pipes/transform-code.pipe';
+import { NavBarComponent } from './shared/nav-bar';
+import { NotFoundComponent } from './core/components/not-found';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     CourseListComponent,
     StarComponent,
     TransformCodePipe,
+    NavBarComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
